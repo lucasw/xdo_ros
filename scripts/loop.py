21fd8d07af2330fd7a8acd6e941c404818e13792
Lucas Walter <wsacul@gmail.com>
2016-03-13 23:38:36 -0700

    Was able to demonstrate control in minecraft- the window has to be selected manually, then the key events take over.

--- scripts/loop.py
+++ scripts/loop.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
 
 import rospy
+from opencv_apps.msg import Point2D
 from std_msgs.msg import String
 
-pub = rospy.Publisher("string", String, queue_size=1)
+rospy.init_node('loop_and_send_keys')
+down_pub = rospy.Publisher("keys_down", String, queue_size=1)
+up_pub = rospy.Publisher("keys_up", String, queue_size=1)
+mouse_rel_pub = rospy.Publisher("mouse_rel", Point2D, queue_size=1)
 
+down_pub.publish(String("w"))
 while not rospy.is_shutdown():
-    pub.publish(String("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwddddddddddddd"))
-    rospy.Duration(0.2).sleep()
+    # mouse_rel_pub.publish(Point2D(200,0))
+    # rospy.sleep(2.0)
+    # continue
+
+    down_pub.publish(String("w"))
+    rospy.sleep(2.0)
+    up_pub.publish(String("w"))
+
+    down_pub.publish(String("d"))
+    rospy.sleep(2)
+    up_pub.publish(String("d"))
+
+    down_pub.publish(String("w"))
+    rospy.sleep(2.0)
+    up_pub.publish(String("w"))
+
+    down_pub.publish(String("a"))
+    rospy.sleep(2)
+    up_pub.publish(String("a"))
+
+    mouse_rel_pub.publish(Point2D(200,0))
--- src/xdo_ros.cpp
+++ src/xdo_ros.cpp
@@ -16,9 +16,15 @@ public:
 protected:
   ros::NodeHandle nh_;
   ros::Subscriber point_sub_;
-  ros::Subscriber string_sub_;
   void mousePosCallback(const opencv_apps::Point2D::ConstPtr& msg);
+  ros::Subscriber mouse_rel_sub_;
+  void mouseRelCallback(const opencv_apps::Point2D::ConstPtr& msg);
+  ros::Subscriber string_sub_;
   void stringCallback(const std_msgs::String::ConstPtr& msg);
+  ros::Subscriber keys_down_sub_;
+  void keysDownCallback(const std_msgs::String::ConstPtr& msg);
+  ros::Subscriber keys_up_sub_;
+  void keysUpCallback(const std_msgs::String::ConstPtr& msg);
   xdo_t* xdo_;
   Window window_;
 };
@@ -29,7 +35,10 @@ XdoRos::XdoRos()
   window_ = CURRENTWINDOW;
   // TODO(lucasw) check xdo for success?
   point_sub_ = nh_.subscribe("mouse_pos", 1, &XdoRos::mousePosCallback, this);
+  mouse_rel_sub_ = nh_.subscribe("mouse_rel", 1, &XdoRos::mouseRelCallback, this);
   string_sub_ = nh_.subscribe("string", 1, &XdoRos::stringCallback, this);
+  keys_down_sub_ = nh_.subscribe("keys_down", 1, &XdoRos::keysDownCallback, this);
+  keys_up_sub_ = nh_.subscribe("keys_up", 1, &XdoRos::keysUpCallback, this);
 }
 
 void XdoRos::mousePosCallback(const opencv_apps::Point2D::ConstPtr& msg)
@@ -37,9 +46,24 @@ void XdoRos::mousePosCallback(const opencv_apps::Point2D::ConstPtr& msg)
   xdo_move_mouse(xdo_, msg->x, msg->y, 0);
 }
 
+void XdoRos::mouseRelCallback(const opencv_apps::Point2D::ConstPtr& msg)
+{
+  xdo_move_mouse_relative(xdo_, msg->x, msg->y);
+}
+
 void XdoRos::stringCallback(const std_msgs::String::ConstPtr& msg)
 {
   xdo_enter_text_window(xdo_, window_, msg->data.c_str(), 12000);
+}
+
+void XdoRos::keysDownCallback(const std_msgs::String::ConstPtr& msg)
+{
+  xdo_send_keysequence_window_down(xdo_, window_, msg->data.c_str(), 12000);
+}
+
+void XdoRos::keysUpCallback(const std_msgs::String::ConstPtr& msg)
+{
+  xdo_send_keysequence_window_up(xdo_, window_, msg->data.c_str(), 12000);
 }
 
 int main(int argc, char** argv)
